d7fc2ab79783ceaa532209b26df39451b045d0f8
Chiel ten Brinke <ctenbrinke@gmail.com>
2022-01-11 12:52:11 +0100

    split up

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 yamltotm/yamltotm.py templates/ansi16.yaml -d palettes/material-dark.yaml output/material-dark-ansi16.tmTheme
-python3 yamltotm/yamltotm.py templates/ansi16.yaml -d palettes/ansi-terminal.yaml output/ansi16.tmTheme
+python3 yamltotm/yamltotm.py -d palettes/material-dark.yaml ansi16.header.yaml templates/ansi16*.yaml output/material-dark-ansi16.tmTheme
+python3 yamltotm/yamltotm.py -d palettes/ansi-terminal.yaml ansi16.header.yaml templates/ansi16*.yaml output/ansi16.tmTheme
 python3 yamltotm/yamltotm.py templates/vscode-synthwave.json -d palettes/material-dark.yaml -r output/vscode-synthwave-material-dark-ansi16.json
