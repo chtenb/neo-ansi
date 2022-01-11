3f975cae56c34f84b410e9b14a7e645ec4bccef6
Chiel ten Brinke <ctenbrinke@gmail.com>
2022-01-11 10:30:41 +0100

    add vscode template

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python3 yamltotm/yamltotm.py templates/ansi16.template.yaml -d palettes/material-dark.palette.yaml output/material-dark-ansi16.tmTheme
-python3 yamltotm/yamltotm.py templates/ansi16.template.yaml -d palettes/ansi-terminal.palette.yaml output/ansi16.tmTheme
+python3 yamltotm/yamltotm.py templates/ansi16.yaml -d palettes/material-dark.yaml output/material-dark-ansi16.tmTheme
+python3 yamltotm/yamltotm.py templates/ansi16.yaml -d palettes/ansi-terminal.yaml output/ansi16.tmTheme
+python3 yamltotm/yamltotm.py templates/vscode-synthwave.json -d palettes/material-dark.yaml -r output/vscode-synthwave-material-dark-ansi16.json
