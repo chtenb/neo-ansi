ac99ccf35affbd6577be0c02eebd8490986aa90e
Chiel ten Brinke <ctenbrinke@gmail.com>
2022-01-11 09:44:17 +0100

    move

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 yamltotm/yamltotm.py ansi16.template.yaml -d material-dark.palette.yaml material-dark-ansi16.tmTheme
-python3 yamltotm/yamltotm.py ansi16.template.yaml -d ansi-terminal.palette.yaml ansi16.tmTheme
+python3 yamltotm/yamltotm.py templates/ansi16.template.yaml -d palettes/material-dark.palette.yaml output/material-dark-ansi16.tmTheme
+python3 yamltotm/yamltotm.py templates/ansi16.template.yaml -d palettes/ansi-terminal.palette.yaml output/ansi16.tmTheme
