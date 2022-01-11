c76ac221336c26f36a2a89363454c01b0ca6a9ba
Chiel ten Brinke <ctenbrinke@gmail.com>
2022-01-11 09:40:38 +0100

    update

---
+++ generate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 yamltotm/yamltotm.py ansi16.template.yaml -d material-dark.palette.yaml material-dark-ansi16.tmTheme
+python3 yamltotm/yamltotm.py ansi16.template.yaml -d ansi-terminal.palette.yaml ansi16.tmTheme
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 yamltotm/yamltotm.py -d material-dark.yaml ansi16.yaml material-dark-ansi16.tmTheme
