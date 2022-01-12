5aeb937d896beaf386c863efcffbd469509fd0af
Chiel ten Brinke <ctenbrinke@gmail.com>
2022-01-12 11:31:28 +0100

    lux

--- generate.sh
+++ generate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 python3 yamltotm/yamltotm.py -d palettes/material-dark.yaml ansi16.header.yaml templates/ansi16*.yaml output/material-dark-ansi16.tmTheme
+python3 yamltotm/yamltotm.py -d palettes/material-light.yaml ansi16.header.yaml templates/ansi16*.yaml output/material-light-ansi16.tmTheme
 python3 yamltotm/yamltotm.py -d palettes/ansi-terminal.yaml ansi16.header.yaml templates/ansi16*.yaml output/ansi16.tmTheme
 python3 yamltotm/yamltotm.py templates/vscode-synthwave.json -d palettes/material-dark.yaml -r output/vscode-synthwave-material-dark-ansi16.json
+python3 yamltotm/yamltotm.py templates/vscode-lux.json -d palettes/material-light.yaml -r output/vscode-lux-material-light-ansi16.json
