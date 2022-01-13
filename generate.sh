f2d741b0d395dee4f5da2ab98d1bb88e2d5d0d7b
Chiel ten Brinke <ctenbrinke@gmail.com>
2022-01-13 13:06:34 +0100

    update

--- generate.sh
+++ generate.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 python3 yamltotm/yamltotm.py -d palettes/material-dark.yaml ansi16.header.yaml templates/ansi16*.yaml output/material-dark-ansi16.tmTheme
 python3 yamltotm/yamltotm.py -d palettes/material-light.yaml ansi16.header.yaml templates/ansi16*.yaml output/material-light-ansi16.tmTheme
-python3 yamltotm/yamltotm.py -d palettes/lux-light.yaml ansi16.header.yaml templates/ansi16*.yaml output/lux-light-ansi16.tmTheme
-python3 yamltotm/yamltotm.py -d palettes/ansi-terminal.yaml ansi16.header.yaml templates/ansi16*.yaml output/ansi16.tmTheme
-python3 yamltotm/yamltotm.py templates/vscode-synthwave.json -d palettes/material-dark.yaml -r output/vscode-synthwave-material-dark-ansi16.json
-#python3 yamltotm/yamltotm.py templates/vscode-lux.json -d palettes/material-light.yaml -r output/vscode-lux-material-light-ansi16.json
-python3 yamltotm/yamltotm.py templates/vscode-lux.json -d palettes/lux-light.yaml -r output/vscode-lux-light-ansi16.json
+python3 yamltotm/yamltotm.py -d palettes/noctis-lux.yaml ansi16.header.yaml templates/ansi16*.yaml output/noctis-lux-ansi16.tmTheme
+python3 yamltotm/yamltotm.py -d palettes/terminal.yaml ansi16.header.yaml templates/ansi16*.yaml output/ansi16.tmTheme
