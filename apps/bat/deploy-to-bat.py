192a0623de7938d321d073bb96ad970e014388fe
chtenb <ctenbrinke@gmail.com>
2024-08-28 11:24:08 +0200

    make dir when not exists

--- apps/bat/deploy-to-bat.py
+++ apps/bat/deploy-to-bat.py
@@ -19,6 +19,7 @@ current_dir = Path(os.path.dirname(os.path.abspath(__file__)))
 file_path = current_dir / '../../syntax/terminal-neo-ansi.tmTheme'
 config_dir = Path(get('bat', '--config-dir').strip())
 themes_dir = config_dir / 'themes'
+os.makedirs(themes_dir, exist_ok=True)
 print(f'Copying {file_path} to {themes_dir}')
 shutil.copy(file_path, themes_dir)
 
